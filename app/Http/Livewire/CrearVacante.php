f1c2bfbdc96eb28620098e0138841b529fa187e5
Raul <raulramirezolmedo@gmail.com>
2023-03-16 21:56:51 -0300

    Se finaliza la clase 210

--- app/Http/Livewire/CrearVacante.php
+++ app/Http/Livewire/CrearVacante.php
@@ -4,6 +4,7 @@ namespace App\Http\Livewire;
 
 use App\Models\Categoria;
 use App\Models\Salario;
+use App\Models\Vacante;
 use Livewire\Component;
 use Livewire\WithFileUploads;
 
@@ -32,6 +33,25 @@ class CrearVacante extends Component
     public function crearVacante()
     {
         $datos = $this->validate();
+
+        // Almacenar la imagen
+        $imagen = $this->imagen->store('public/vacantes');
+        $datos['imagen'] = str_replace('public/vacantes/', '', $imagen);
+        // Crear la Vacante
+        Vacante::create([
+            'titulo' => $datos['titulo'],
+            'salario_id' => $datos['salario'],
+            'categoria_id' => $datos['categoria'],
+            'empresa' => $datos['empresa'],
+            'ultimo_dia' => $datos['ultimo_dia'],
+            'descripcion' => $datos['descripcion'],
+            'imagen' => $datos['imagen'],
+            'user_id' => auth()->user()->id
+        ]);
+        // Crear mensaje
+        session()->flash('mensaje', 'La vacante se ha publicado correctamente.');
+        // Redireccionar al usuario
+        return redirect()->route('vacantes.index');
     }
 
     public function render()
