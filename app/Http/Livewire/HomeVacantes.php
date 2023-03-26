07aab8c89ed995b1ace3afb084e14ff94c5b39a8
Raul <raulramirezolmedo@gmail.com>
2023-03-26 10:12:12 -0400

    se finaliza la clase 253

---
+++ app/Http/Livewire/HomeVacantes.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Vacante;
+use Livewire\Component;
+
+class HomeVacantes extends Component
+{
+    public function render()
+    {
+        $vacantes = Vacante::all();
+        return view(
+            'livewire.home-vacantes',
+            [
+                'vacantes' => $vacantes
+            ]
+        );
+    }
+}
